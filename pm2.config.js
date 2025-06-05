127ec2a9ab3c122bd8ede5108c6b40ae1acbbc21
MythEclipse <asepharyanalm001@gmail.com>
2025-06-05 14:18:22 +0700

    a

---
+++ pm2.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name   : "ekraf-pm2", 
+    script : "npm", // atau "yarn"
+    args   : "start", // Skrip start Anda mungkin sudah menghargai variabel PORT dari .env.production
+    // Atau, jika Anda perlu secara eksplisit mengatur port melalui argumen:
+    // script : "node_modules/next/dist/bin/next",
+    // args   : "start -p 4097",
+    watch: false,
+    max_memory_restart: '1G',
+    env_production: {
+       NODE_ENV: "production",
+       PORT: 4097 
+    },
+  }]
+}
