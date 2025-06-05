e9d2330f53d7166b0ee794f642de505b309df4dd
MythEclipse <asepharyanalm001@gmail.com>
2025-06-05 15:12:20 +0700

    a

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name   : "ekraf", 
     script : "npm", // atau "yarn"
-    args   : "start", // Skrip start Anda mungkin sudah menghargai variabel PORT dari .env.production
+    args   : "start -p 4097", // Skrip start Anda mungkin sudah menghargai variabel PORT dari .env.production
     // Atau, jika Anda perlu secara eksplisit mengatur port melalui argumen:
     // script : "node_modules/next/dist/bin/next",
     // args   : "start -p 4097",
