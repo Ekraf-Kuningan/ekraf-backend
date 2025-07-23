5e8adf7546169c7154ed032763d9e68f6d0eee22
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2025-07-24 01:58:10 +0700

    Update SwaggerUIComponent.tsx

--- app/SwaggerUIComponent.tsx
+++ app/SwaggerUIComponent.tsx
@@ -5,7 +5,7 @@ import type { OpenAPIV3 } from 'openapi-types';
 import { useTheme } from 'next-themes';
 import 'swagger-ui-dist/swagger-ui.css';
 import './swagger-dark.css';
-// @ts-expect-error: No official types for swagger-ui-dist bundle
+// @ts-expect-error: No official types for swagger-ui-dist bundlea
 import { SwaggerUIBundle } from 'swagger-ui-dist';
 
 interface SwaggerUIComponentProps {
